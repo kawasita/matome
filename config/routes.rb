bc14f13dd11450a8910f6077d2a212e3b50925cb
EC2 Default User <ec2-user@ip-172-31-12-34.ap-northeast-1.compute.internal>
2023-02-01 15:00:00 +0000

    [add]routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
-  get 'homes/top'
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: "homes#top"
+  
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    resources :book_comments, only: [:create, :destroy]
+    resource :bookmarks, only: [:create, :destroy]
+  end
+  get 'bookmarks' => 'bookmarks#index'
+  
+  resources :users, only: [:index,:show,:edit,:update]do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
