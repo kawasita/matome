916f896975260c2004a0681ebfa11a12728fc256
EC2 Default User <ec2-user@ip-172-31-12-34.ap-northeast-1.compute.internal>
2023-02-03 14:23:09 +0000

    [problem]comment非同期

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,6 +7,8 @@ class BooksController < ApplicationController
     
     def show
       @book = Book.find(params[:id])
+      @book_new = Book.new
+      @book_comment = BookComment.new
     end
     
     def new
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,11 @@
 class Book < ApplicationRecord
+    
+  belongs_to :user
+  has_many :bookmarks, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
+  
+  def bookmarked_by?(user)
+    bookmarks.where(user_id: user).exists?
+  end
+    
 end
