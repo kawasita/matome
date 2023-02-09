9aaa76f41946b75fd994329591382988430506b2
kawasita <115086200+kawasita@users.noreply.github.com>
2023-02-09 13:36:19 +0000

    [update]book/update機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,6 +15,19 @@ class BooksController < ApplicationController
       @book = Book.new
     end
     
+    def edit
+      @book = Book.find(params[:id])
+    end
+    
+    def update
+      @book = Book.find(params[:id])
+      if @book.update(book_params)
+        redirect_to book_path(@book)
+      else
+        render "edit"
+      end
+    end
+    
     def create
       book = Book.new(book_params)
       book.user_id = current_user.id
